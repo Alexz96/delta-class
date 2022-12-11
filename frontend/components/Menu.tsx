f3227915c54b07fcf2bbccdf2d34e87d8a80c50f
Alexsander <alexsander1996@yahoo.com.br>
2022-12-11 14:29:57 -0300

    Novo componente e sua estilização. Nova classe base para gerenciamento.

---
+++ frontend/components/Menu.tsx
@@ -0,0 +1,21 @@
+import styles from "../styles/Menu.module.css";
+
+interface MenuOptionsProps {
+  texto: string;
+  href: string;
+}
+
+export default function Menu(options: MenuOptionsProps) {
+
+    // function renderOptions(){
+        
+    // }
+  return (
+    <div className={styles.menu}>
+      <h1>Componente Menu</h1>
+        <div>
+
+        </div>
+    </div>
+  );
+}
