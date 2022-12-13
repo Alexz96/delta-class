0b632e0694f82fd023bf173f03e687930c2135e4
Alexsander <alexsander1996@yahoo.com.br>
2022-12-13 19:45:42 -0300

    Frontend com envio de dados parcialmente OK

--- frontend/pages/newstudent.tsx
+++ frontend/pages/newstudent.tsx
@@ -3,7 +3,6 @@ import FormStudent from "../components/FormStudent";
 export default function NewStudent() {
     return (
         <div>
-            <h1>Adicionar aluno</h1>
             <FormStudent />
         </div>
     )
